373f164d7e815757b1eeb90bf670120726b8019a
RanjithSatla <ranjithsatla327@gmail.com>
2022-03-06 18:31:40 +0530

    Add skill and Edit Username if userexits

--- 2.js
+++ 2.js
@@ -50,17 +50,17 @@ var users = [
   },
 ];
 
-// const scoresGreaterThan85 = () => {
-//   for (let i = 0; i < users.length; i++) {
-//     if (users[i].scores > 85) {
-//       //  console.log(users[i]);
-//     }
-//   }
-// };
+const scoresGreaterThan85 = () => {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].scores > 85) {
+      //  console.log(users[i]);
+    }
+  }
+};
 
-// scoresGreaterThan85(users);
+scoresGreaterThan85(users);
 
-// 	Write a function which addUser  to the user array only if the user does not exist.
+// Write a function which addUser  to the user array only if the user does not exist.
 
 const addUser = (users, user) => {
   let exists = false;
@@ -84,9 +84,46 @@ const addUser = (users, user) => {
 
 const user = {
   name: "Ram",
-  scores: 90,
-  skills: ["HTM", "CSS", "JS"],
-  age: 20,
 };
 
 addUser(users, user);
+
+////// Add User Skill ///////
+
+const addUserSkill = (users, skill, username) => {
+  let exists = false;
+
+  for (let i = 0; i < users.length; i++) {
+    // console.log(user.name);
+    if (users[i].name == username) {
+      exists = true;
+      console.log("User exists");
+      users[i].skills.push(skill);
+      console.log(users);
+    }
+  }
+};
+
+const skills = "Java";
+
+addUserSkill(users, skills, "Thomas");
+
+/////// Edit UserName ///////
+
+const editUserName = (users, username, editName) => {
+  let exists = false;
+
+  for (let i = 0; i < users.length; i++) {
+    // console.log(user.name);
+    if (users[i].name == username) {
+      exists = true;
+      console.log("User exists");
+      users[i].name = editName;
+      console.log(users);
+    }
+  }
+};
+
+const uname = "Martha";
+
+editUserName(users, uname, "Parata");
